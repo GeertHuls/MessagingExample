4256b219bf9b0fc5c6ecfe563dbbb9f2f7cc413e
Geert Huls <geert.huls82@gmail.com>
2016-07-06 12:06:05 +0200

    Initial setup - Solution file - Web app

---
+++ WheelsOnFireWeb/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WheelsOnFireWeb.ViewModels;
+
+namespace WheelsOnFireWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult RegisterOrder()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RegisterOrder(OrderViewModel model)
+        {
+            //var registerOrderCommand = new RegisterOrderCommand(model);
+
+            //Send RegisterOrderCommand
+
+            return View("Thanks");
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
