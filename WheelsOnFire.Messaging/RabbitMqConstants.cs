d8c013b964867f0926076c950ccc66337d42927c
Geert Huls <geert.huls82@gmail.com>
2016-07-12 23:38:52 +0200

    Amqp connection string changed

--- WheelsOnFire.Messaging/RabbitMqConstants.cs
+++ WheelsOnFire.Messaging/RabbitMqConstants.cs
@@ -2,7 +2,7 @@
 {
     public static class RabbitMqConstants
     {
-        public const string RabbitMqUri = "amqp://guest:guest@localhost:5672/";
+        public const string RabbitMqUri = "amqp://guest:guest@lnxvm:5672/";
         public const string JsonMimeType = "application/json";
 
         public const string RegisterOrderExchange = "fireonwheels.registerorder.exchange";
