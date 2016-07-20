e9ce7410abe02ad89db038c17bb0dc9e4211470f
Geert Huls <geert.huls82@gmail.com>
2016-07-20 21:39:46 +0200

    Registration console

---
+++ WheelsOnFire.Registration.Console/RabbitMqManager.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Text;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using WheelsOnFire.Messaging;
+
+namespace WheelsOnFire.Registration.Console
+{
+    public class RabbitMqManager: IDisposable
+    {
+        private readonly IModel channel;
+
+        public RabbitMqManager()
+        {
+            var connectionFactory = 
+                new ConnectionFactory {Uri = RabbitMqConstants.RabbitMqUri};
+            var connection = connectionFactory.CreateConnection();
+            channel = connection.CreateModel();
+            connection.AutoClose = true;
+        }
+
+        public void ListenForRegisterOrderCommand()
+        {
+            channel.QueueDeclare(
+                queue: RabbitMqConstants.RegisterOrderQueue, 
+                durable: false, exclusive: false,
+                autoDelete: false, arguments: null);
+
+            //Prefetch gets multiple messages at once and process them in-memory.
+            //Prefetch size set to 0 means no limit.
+            channel.BasicQos(prefetchSize: 0, prefetchCount: 1, 
+                global: false);
+
+            //Bind order command to the queue:
+            var consumer = new RegisteredOrderCommandConsumer(this);
+            channel.BasicConsume(
+                queue: RabbitMqConstants.RegisterOrderQueue,
+                noAck: false,
+                consumer: consumer);
+        }
+
+        public void SendOrderRegisteredEvent(IOrderRegisteredEvent command)
+        {
+            channel.ExchangeDeclare(
+                exchange: RabbitMqConstants.OrderRegisteredExchange, 
+                type: ExchangeType.Fanout); //Using exchange type fanout instead of direct.
+                                //Fanout because this exchange is meant to send out messages
+                                //to muliple queues.
+            channel.QueueDeclare(
+                queue: RabbitMqConstants.OrderRegisteredNotificationQueue, 
+                durable: false, exclusive: false,
+                autoDelete: false, arguments: null);
+            channel.QueueBind(
+                queue: RabbitMqConstants.OrderRegisteredNotificationQueue,
+                exchange: RabbitMqConstants.OrderRegisteredExchange, 
+                routingKey: "");
+
+            var serializedCommand = JsonConvert.SerializeObject(command);
+
+            var messageProperties = channel.CreateBasicProperties();
+            messageProperties.ContentType = RabbitMqConstants.JsonMimeType;
+
+            channel.BasicPublish(
+                exchange: RabbitMqConstants.OrderRegisteredExchange, 
+                routingKey: "",
+                basicProperties: messageProperties, 
+                body: Encoding.UTF8.GetBytes(serializedCommand));
+        }
+
+        public void SendAck(ulong deliveryTag)
+        {
+            //When rabbit mq receives this acknowledgement, it will be deleted from the queue.
+            channel.BasicAck(deliveryTag: deliveryTag, multiple: false);
+        }
+
+        public void Dispose()
+        {
+            if (!channel.IsClosed)
+                channel.Close();
+        }
+    }
+}
---
+++ WheelsOnFire.Registration.Console/RegisterOrderCommandConsumer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using WheelsOnFire.Messaging;
+using WheelsOnFire.Registration.Console.Messages;
+
+namespace WheelsOnFire.Registration.Console
+{
+    public class RegisteredOrderCommandConsumer: DefaultBasicConsumer
+    {
+        private readonly RabbitMqManager _rabbitMqManager;
+
+        public RegisteredOrderCommandConsumer(
+            RabbitMqManager rabbitMqManager)
+        {
+            this._rabbitMqManager = rabbitMqManager;
+        }
+
+        public override void HandleBasicDeliver(
+            string consumerTag, ulong deliveryTag, 
+            bool redelivered, string exchange, string routingKey,
+            IBasicProperties properties, byte[] body)
+        {
+            if (properties.ContentType != RabbitMqConstants.JsonMimeType)
+                throw new ArgumentException(
+                    $"Can't handle content type {properties.ContentType}");
+
+            var message = Encoding.UTF8.GetString(body);
+            var commandObj = 
+                JsonConvert.DeserializeObject<RegisterOrderCommand>(
+                    message);
+            Consume(commandObj);
+            //The delivery tag is unique.
+            _rabbitMqManager.SendAck(deliveryTag);
+        }
+
+        private void Consume(IRegisterOrderCommand command)
+        {
+            //Store order registration and get Id
+            var id = 12;
+
+            System.Console.WriteLine($"Order with id {id} registered");
+            System.Console.WriteLine("Publishing order registered event");
+
+            //notify subscribers that a order is registered
+            var orderRegisteredEvent = new OrderRegisteredEvent(command, id);
+            //publish event
+            _rabbitMqManager.SendOrderRegisteredEvent(orderRegisteredEvent);
+        }
+    }
+}
