5e6fa82051b03590045f7a51abe9b2165a3c18bc
Geert Huls <geert.huls82@gmail.com>
2016-07-24 22:22:18 +0200

    Added finance console app as receiver of order registered events

---
+++ WheelsOnFire.Finance.Console/OrderRegisteredConsumer.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using MassTransit;
+using WheelsOnFire.Messaging;
+
+namespace WheelsOnFire.Finance.Console
+{
+    public class OrderRegisteredConsumer : IConsumer<IOrderRegisteredEvent>
+    {
+        public async Task Consume(ConsumeContext<IOrderRegisteredEvent> context)
+        {
+            //Save to db
+            await System.Console.Out.WriteLineAsync($"New order received: Order id {context.Message.OrderId}");
+        }
+    }
+}
---
+++ WheelsOnFire.Finance.Console/Program.cs
@@ -0,0 +1,28 @@
+﻿using MassTransit;
+using WheelsOnFire.Messaging;
+
+namespace WheelsOnFire.Finance.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Title = "Finance";
+
+            var bus = BusConfigurator.ConfigureBus((cfg, host) =>
+            {
+                cfg.ReceiveEndpoint(host, RabbitMqConstants.FinanceServiceQueue, e =>
+                {
+                    e.Consumer<OrderRegisteredConsumer>();
+                });
+            });
+
+            bus.Start();
+
+            System.Console.WriteLine("Listening for Order registered events.. Press enter to exit");
+            System.Console.ReadLine();
+
+            bus.Stop();
+        }
+    }
+}
